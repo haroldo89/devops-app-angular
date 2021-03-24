a1691612951f4b2f8078a6bbeb48084ce71fc46d
hacaicedot <hacaicedot@correo.udistrital.edu.co>
2021-03-24 13:58:03 -0500

    changes aplication with login

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  baseUrl: 'http://localhost:59577',
+  firebase: {
+    apiKey: "AIzaSyC8RKGRyLF7DC30xWeWHeGkrW1KzODDGak",
+    authDomain: "taller-f6380.firebaseapp.com",
+    databaseURL: "https://taller-f6380.firebaseio.com",
+    projectId: "taller-f6380",
+    storageBucket: "taller-f6380.appspot.com",
+    messagingSenderId: "100908753714",
+    appId: "1:100908753714:web:45dc651c89e49a66d66d2b"
+  }
 };
