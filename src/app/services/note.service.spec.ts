c464d31d4cf02eb87150bbc91906c10c7cc3672c
haroldo89 <haroldo_89@hotmail.com>
2021-11-04 10:14:53 -0500

    changes to test the unit test for angular

--- src/app/services/note.service.spec.ts
+++ src/app/services/note.service.spec.ts
@@ -1,12 +1,22 @@
+import { HttpClient } from '@angular/common/http';
 import { TestBed } from '@angular/core/testing';
+import { AngularFireModule } from '@angular/fire';
+import { AngularFireDatabaseModule } from '@angular/fire/database';
+import { environment } from '../../environments/environment';
 
 import { NoteService } from './note.service';
+import { TokenService } from './token.service';
 
 describe('NoteService', () => {
   let service: NoteService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      AngularFireModule.initializeApp(environment.firebase),
+      AngularFireDatabaseModule,
+      HttpClient,
+      TokenService
+    });
     service = TestBed.inject(NoteService);
   });
 
--- src/app/services/token.service.spec.ts
+++ src/app/services/token.service.spec.ts
@@ -1,12 +1,22 @@
 import { TestBed } from '@angular/core/testing';
-
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { TokenService } from './token.service';
+import { AngularFireModule } from '@angular/fire';
+import { environment } from 'src/environments/environment';
+import { AngularFireDatabaseModule } from '@angular/fire/database';
 
 describe('TokenService', () => {
   let service: TokenService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [
+        AngularFireModule.initializeApp(environment.firebase),
+        AngularFireDatabaseModule,
+        HttpClient,
+        TokenService
+      ]
+    });
     service = TestBed.inject(TokenService);
   });
 
